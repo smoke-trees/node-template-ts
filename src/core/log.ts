67310ec1ae9db45e15088a494a38ce06c30e9fe2
Anshuman Chhapolia <anshuman@smoketrees.in>
2021-11-22 21:36:24 +0530

    feat: add oops to project
    Signed-off-by: Anshuman Chhapolia <anshuman@smoketrees.in>

--- src/core/log.ts
+++ src/core/log.ts
@@ -4,7 +4,6 @@ import { ContextProvider } from '@smoke-trees/smoke-context'
 const contextFormat = format((info, { opts }) => {
   const context = ContextProvider.getContext()
   info.traceId = context?.traceId
-  console.log(context)
   return info
 })
 
@@ -14,7 +13,7 @@ const wTransports = [
     handleExceptions: true,
     format: process.env.NODE_ENV === 'production'
       ? format.combine(contextFormat(), format.timestamp(), format.json())
-      : format.combine(contextFormat(), format.colorize(), format.simple())
+      : format.combine(contextFormat(), format.timestamp(), format.colorize(), format.simple())
   })
 ]
 
@@ -39,19 +38,19 @@ class CustomLogger {
     this._logger = logger
   }
 
-  info(message: string, functionName?: string, ...meta: any[]): void {
+  info(message: string, functionName?: string, meta?: any): void {
     this._logger.info(message, { functionName, ...meta })
   }
 
-  debug(message: string, functionName?: string, ...meta: any[]): void {
+  debug(message: string, functionName?: string, meta?: any): void {
     this._logger.debug(message, { functionName, ...meta })
   }
 
-  trace(message: string, functionName?: string, ...meta: any[]): void {
+  trace(message: string, functionName?: string, meta?: any): void {
     this._logger.error(message, { functionName, ...meta })
   }
 
-  error(message: string, functionName?: string, error?: unknown, ...meta: any[]): void {
+  error(message: string, functionName?: string, error?: unknown, meta?: any): void {
     this._logger.error(message, {
       error: error instanceof Error && error.stack,
       functionName,
@@ -59,11 +58,11 @@ class CustomLogger {
     })
   }
 
-  warn(message: string, functionName?: string, ...meta: any[]): void {
+  warn(message: string, functionName?: string, meta?: any): void {
     this._logger.warn(message, { functionName, ...meta })
   }
 
-  fatal(message: string, functionName?: string, ...meta: any[]): void {
+  fatal(message: string, functionName?: string, meta?: any): void {
     this._logger.error(message, { functionName, ...meta })
   }
 }
---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import http from 'http'
+import TestController from './app/test'
+import Application from './core/app'
+
+
+const app = new Application()
+
+
+app.addController(new TestController(app))
+
+app.loadControllers()
+
+app.run()
+
+
