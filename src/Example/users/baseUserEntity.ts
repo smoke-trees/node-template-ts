c6773a127708215f92cde74a7ad2843af73f9459
Anshuman Chhapolia <anshuman@smoketrees.in>
2022-09-26 17:59:27 +0530

    fix: tests
    Signed-off-by: Anshuman Chhapolia <anshuman@smoketrees.in>

--- src/Example/users/baseUserEntity.ts
+++ src/Example/users/baseUserEntity.ts
@@ -5,9 +5,9 @@ import { IUser } from "./IUser";
 // @Entity({ name: 'user_test_table' })
 @Documentation.addSchema({ type: 'object' })
 export class BaseUser extends BaseEntity  {
-  @Column('varchar', { name: 'name_user', length: 255 })
-  @Validator({ required: true, updatable: true })
-  @Documentation.addField({ type: 'string', minLength: 3, maxLength: 255 })
+  @Column('varchar', { name: 'name_user_1', length: 255, nullable: true })
+  @Validator({ required: false, updatable: false })
+  @Documentation.addField({ type: 'string', minLength: 3, maxLength: 255, nullable: true })
   lastName!: string;
 
   constructor(it?: IUser) {
