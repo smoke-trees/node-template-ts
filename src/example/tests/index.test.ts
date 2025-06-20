f6b89242eb52bb25dcf76f09d27a44eb616442d8
Anshuman Chhapolia <anshuman@smoketrees.in>
2025-06-21 00:11:39 +0530

    feat: improve test to follow
    Signed-off-by: Anshuman Chhapolia <anshuman@smoketrees.in>

--- src/example/tests/index.test.ts
+++ src/example/tests/index.test.ts
@@ -1,26 +1,11 @@
 import { Application, Database } from '@smoke-trees/postgres-backend'
-import { ContextProvider } from '@smoke-trees/smoke-context'
-import compression from 'compression'
-import express from 'express'
-import settings from '../../settings'
-import { User, UserController, UserDao, UserService } from '../app/users'
+import { container } from '../setup'
 import { ExampleControllerTest } from './app/ExampleTests/controller.test'
 import { ExampleServiceTest } from './app/ExampleTests/services.test'
 import { clearUserTable } from './utils/clear-database.test'
 
-const database = new Database(settings)
-database.addEntity(User)
-const app = new Application(settings, database)
-
-app.addMiddleWare(ContextProvider.getMiddleware({ headerName: 'X-Request-ID' }))
-app.addMiddleWare(compression())
-app.addMiddleWare(express.json({}))
-
-const userDao = new UserDao(database)
-const userService = new UserService(userDao)
-const userController = new UserController(app, userService)
-
-app.addController(userController)
+const database = container.get<Database>('database')
+const app = container.get(Application)
 
 app.loadMiddleware()
 app.loadControllers()
